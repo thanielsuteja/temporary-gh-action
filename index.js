e19f157ad87f7fb84658f4a270168e3cba4ae109
Thaniel Suteja <thaniel.suteja@traveloka.com>
2024-09-16 22:13:49 +0700

    fix undefined error log

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ async function run() {
         core.setOutput('whatever', `${prevArtifact} and ${commitComparison.comparisonUrl}`)
     } catch (error) {
         // Fail the workflow run if an error occurs
-        core.error(`Error: ${error.message}`)
-        core.setFailed(error.message)
+        core.error(`Error: ${error}`)
+        core.setFailed(error)
     }
 }
 
